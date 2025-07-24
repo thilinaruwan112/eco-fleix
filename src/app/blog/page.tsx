695cabac6aea2ca6c8dc9df18fffca664626cf5a
Hansi Senevirathna <englishbasictobrilliance@gmail.com>
2025-07-24 09:47:53 +0000

    add this section after the hero section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,11 @@
 import BlogHero from '@/components/sections/BlogHero';
+import LatestInsights from '@/components/sections/LatestInsights';
 
 export default function BlogPage() {
   return (
     <div className="flex flex-col">
       <BlogHero />
+      <LatestInsights />
     </div>
   );
 }
