c33a2dadf172e52646afb0654df9284af959d2f8
Hansi Senevirathna <englishbasictobrilliance@gmail.com>
2025-08-02 07:52:38 +0000

    add PWA applicaion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export const metadata: Metadata = {
     'computer recycling dubai',
     'laptop recycling dubai',
   ],
+  manifest: '/manifest.webmanifest',
 };
 
 export default function RootLayout({
@@ -75,6 +76,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
         <TranslationProvider>
